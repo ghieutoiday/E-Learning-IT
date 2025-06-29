fc03d65d2e6fe6afba07bf609325d396dde98b02
thehung2711 <hungnthe182170@fpt.edu.vn>
2025-06-29 21:08:24 +0700

    Update 29 6 Hung

--- src/java/controller/ShowRegistrationPopupController.java
+++ src/java/controller/ShowRegistrationPopupController.java
@@ -2,6 +2,7 @@ package controller;
 
 import dal.CourseDAO;
 import dal.PricePackageDAO;
+import dal.RegistrationDAO;
 import dal.UserDAO;
 import java.io.IOException;
 import java.util.List;
@@ -12,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.Course;
 import model.PricePackage;
+import model.Registration;
 import model.User;
 
 @WebServlet(name = "ShowRegistrationPopupServlet", urlPatterns = {"/showRegistration"})
@@ -24,13 +26,21 @@ public class ShowRegistrationPopupController extends HttpServlet {
 
         int courseId = Integer.parseInt(request.getParameter("courseId"));
         String mode = request.getParameter("mode");
+        
+        RegistrationDAO regDAO = new RegistrationDAO(); // Khởi tạo DAO
 
         if ("loggedIn".equals(mode)) {
-            // TRƯỜNG HỢP 1: Giả lập đã đăng nhập, fix cứng userId=1
-            int currentUserId = 1;
+            int currentUserId = 1; // Giả lập đã đăng nhập, fix cứng userId=1
             UserDAO userDAO = new UserDAO();
             User currentUser = userDAO.getUserByID(currentUserId);
             request.setAttribute("user", currentUser);
+            
+            // LOGIC MỚI: KIỂM TRA ĐƠN ĐĂNG KÝ ĐÃ TỒN TẠI
+            Registration existingReg = regDAO.getRegistrationByUserAndCourse(currentUserId, courseId);
+            if (existingReg != null) {
+                // Nếu tìm thấy, gửi thông tin đơn đăng ký này sang JSP
+                request.setAttribute("existingRegistration", existingReg);
+            }
         }
         
         CourseDAO courseDAO = new CourseDAO();
