86c920d142f5f5497f2425cce675ba33870daab0
ho-quoc <quochhhe187257@fpt.edu.vn>
2025-07-22 21:48:13 +0700

    modify location for QuizLessonDTO

--- src/java/dal/QuizLessonDTO_DAO.java
+++ src/java/dal/QuizLessonDTO_DAO.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import model.DimensionResult;
-import model.QuizLessonDTO;
+import dto.QuizLessonDTO;
 
 /**
  *
